7391c73fadc3961908629cdfe8e254084a40592e
joker <vlady98ish@gmail.com>
2023-07-11 14:55:47 +0300

    1.0.6

--- app/src/main/java/com/example/funlib3/FunFactsLoadingView.java
+++ app/src/main/java/com/example/funlib3/FunFactsLoadingView.java
@@ -36,20 +36,10 @@ public class FunFactsLoadingView extends LinearLayout {
                 R.styleable.FunFactsLoadingView,
                 0, 0);
         Drawable backgroundDrawable;
-        textView = findViewById(R.id.funFactTextView);
+
         try {
             backgroundDrawable = a.getDrawable(R.styleable.FunFactsLoadingView_backgroundImage);
-            float fontSize = a.getDimension(R.styleable.FunFactsLoadingView_customFontSize, 15); // Default size is 15sp
-            textView.setTextSize(fontSize);
 
-            int fontStyle = a.getInt(R.styleable.FunFactsLoadingView_customFontStyle, 0); // Default style is normal
-            textView.setTypeface(null, fontStyle);
-
-            String fontFamily = a.getString(R.styleable.FunFactsLoadingView_customFontFamily); // Default is null
-            if (fontFamily != null) {
-                Typeface typeface = Typeface.create(fontFamily, Typeface.NORMAL);
-                textView.setTypeface(typeface);
-            }
         } finally {
             a.recycle();
         }
@@ -63,8 +53,20 @@ public class FunFactsLoadingView extends LinearLayout {
             inflate(context, R.layout.view_loading, this);
         }
 
+        textView = findViewById(R.id.funFactTextView);
 
+        // These lines should be here, after the textView is initialized.
+        float fontSize = a.getDimension(R.styleable.FunFactsLoadingView_customFontSize, 15); // Default size is 15sp
+        textView.setTextSize(fontSize);
 
+        int fontStyle = a.getInt(R.styleable.FunFactsLoadingView_customFontStyle, 0); // Default style is normal
+        textView.setTypeface(null, fontStyle);
+
+        String fontFamily = a.getString(R.styleable.FunFactsLoadingView_customFontFamily); // Default is null
+        if (fontFamily != null) {
+            Typeface typeface = Typeface.create(fontFamily, Typeface.NORMAL);
+            textView.setTypeface(typeface);
+        }
 
         handler.post(funFactRunnable);
     }
